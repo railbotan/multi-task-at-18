6c4a32aede561749a8abfddb085ea9f92ee846a1
NastyaBay <nastya23700@mail.ru>
2021-11-26 18:41:17 +0500

    cpu-bound_new

--- CPU-bound.py
+++ CPU-bound.py
@@ -14,13 +14,13 @@ def is_prime(n):
 
 
 def main():
-    time1 = time()
     with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
         for answer in zip(executor.map(is_prime, range(3))):
             print(answer)
-    time2 = time()
-    print(time2 - time1)
 
 
 if __name__ == '__main__':
+    time1 = time()
     main()
+    time2 = time()
+    print(time2 - time1)
