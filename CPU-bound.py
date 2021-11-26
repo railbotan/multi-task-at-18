ea972b8dfefb82ca752e68c0c97649116ad2f868
NastyaBay <nastya23700@mail.ru>
2021-11-26 18:36:18 +0500

    cpu-bound_new

--- CPU-bound.py
+++ CPU-bound.py
@@ -1,15 +1,26 @@
 from hashlib import md5
 from random import choice
+import concurrent.futures
+from time import time
 
 
-count = 0
-while True:
-    s = "".join([choice("0123456789") for i in range(50)])
-    h = md5(s.encode('utf8')).hexdigest()
+def is_prime(n):
+    while True:
+        s = "".join([choice("0123456789") for i in range(50)])
+        h = md5(s.encode('utf8')).hexdigest()
 
-    if h.endswith("00000"):
-        print(s, h)
-        count += 1
-        if count == 3:
-            break
+        if h.endswith("00000"):
+            return s + ',' + h
 
+
+def main():
+    time1 = time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+        for answer in zip(executor.map(is_prime, range(3))):
+            print(answer)
+    time2 = time()
+    print(time2 - time1)
+
+
+if __name__ == '__main__':
+    main()
