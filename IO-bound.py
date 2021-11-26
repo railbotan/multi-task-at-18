ac1e9260695a6c77a3285e3dcf5164cfd090b877
NastyaBay <nastya23700@mail.ru>
2021-11-26 13:49:29 +0500

    io-bound1

---
+++ IO-bound.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+
+url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
+
+for i in range(100):
+    s = urlopen(url)
+    print(unquote(s.url))
